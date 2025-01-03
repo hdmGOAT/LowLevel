6cd2057bff28118700a00022c314496a0030d9d9
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-03 12:43:54 +0800

    input integer

---
+++ input.asm
@@ -0,0 +1,12 @@
+.text
+.globl _start
+
+_start:
+    li $v0, 5          
+    syscall             
+    move $t0, $v0      
+    li $v0, 1          
+    move $a0, $t0      
+    syscall            
+    li $v0, 10         
+    syscall
