4fd370b619d9c4fbb2d81e8ff2ae9727108ab625
hdmGOAT <hdmGOAT@users.noreply.github.com>
2025-01-03 23:00:06 +0800

    working file write

--- fileIO.asm
+++ fileIO.asm
@@ -1,7 +1,9 @@
 .text
 _start:
 	li $v0, 13
-	la 
+	la $a0, address
+	li $a1, 9
+	syscall
 	
 	li $v0, 10   # exit syntax 
 	li $a0, 13        
@@ -9,4 +11,4 @@ _start:
 
 .data
 	address:
- 	.asciiz "Test"
+ 	.asciiz "Test.txt"
